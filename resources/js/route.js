a1c3d10b8fb7eb95730c7c4131266fa2da44f700
abdulazizoudah <azoz99aoda@hotmail.com>
2026-03-04 13:46:29 +0300

    clean

--- resources/js/route.js
+++ resources/js/route.js
@@ -2,5 +2,5 @@ import { Ziggy } from './ziggy';
 import * as routeImport from 'ziggy-js';
 
 export const route = (name, params = {}, absolute = true, config = Ziggy) => {
-  return routeImport.route(name, params, absolute, config);
+    return routeImport.route(name, params, absolute, config);
 };
